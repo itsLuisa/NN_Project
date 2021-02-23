fc89b51d2e51fef5027a65e4124a469d07166f17
itsLuisa <lu.geiger3011@gmail.com>
2021-02-23 15:42:21 +0100

    adjusted readme and made get_info more general

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # NN_Project
-Just two students trying to get a neural network to work<br>
+Just two students trying to get a neural network to work.<br>
+More detailed description will follow.
 
 ## Table of contents
 * [General information](#general-information)
@@ -7,14 +8,24 @@ Just two students trying to get a neural network to work<br>
 
 ## General information
 ### Requirements
-* python packages:
-    * collections
+You can either use the file *environment.yaml* to build a conda environment like this:
+```
+$ conda env create --file environment.yaml
+$ conda activate Valentina_Luisa
+```
+or install the following python packages by hand:
+* collections
+* sys
+
+After having set this up, you should be able to run all the code from this repository.
 
 ## Data preprocessing
-* *preprocessing.sh:* shell-script for preprocessing, takes input file as first argument and output file as second argument
-* *sample.tsv:* created by using preprocessing.sh like this:
+* *preprocessing.sh:* shell-script for preprocessing, takes input file as first argument and output file as second argument, like this:
 ```
 $ bash preprocessing.sh sample.conll sample.tsv
 ```
-* *get_information.py:* produces sample.info
-* *sample.info:* includes information about sample.tsv
+* *get_information.py:* uses an input file (sample.tsv) to produce an output file (sample.info), which includes useful information about the dataset.
+Use it like this:
+```
+$ python get_information.py sample.tsv sample.info
+```
--- get_information.py
+++ get_information.py
@@ -1,37 +1,41 @@
 # 1.3 get information about the data
 from collections import defaultdict
+import sys
 
-with open("sample.tsv", encoding="utf-8") as f:
-    sequence_length_list = list()
-    tag_dict = defaultdict(int)
-    for line in f:
-        if "*" in line:
-            sequence_length_list.append(int(previous_line.split()[0]) + 1)
-        else:
-            tag_dict[line.split()[2]] += 1
-        previous_line = line
+def main():
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    with open(input_file, encoding="utf-8") as f:
+        sequence_length_list = list()
+        tag_dict = defaultdict(int)
+        for line in f:
+            if "*" in line:
+                sequence_length_list.append(int(previous_line.split()[0]) + 1)
+            else:
+                tag_dict[line.split()[2]] += 1
+            previous_line = line
 
-print(sequence_length_list)
-max_length = max(sequence_length_list)
-min_length = min(sequence_length_list)
-mean_length = sum(sequence_length_list) / len(sequence_length_list)
-number_seq = len(sequence_length_list)
+    max_length = max(sequence_length_list)
+    min_length = min(sequence_length_list)
+    mean_length = sum(sequence_length_list) / len(sequence_length_list)
+    number_seq = len(sequence_length_list)
 
-print(tag_dict)
-
-with open("sample.info", "w", encoding="utf-8") as f:
-    line = "Max sequence length: " + str(max_length) + "\n"
-    f.writelines(line)
-    line = "Min sequence length: " + str(min_length) + "\n"
-    f.writelines(line)
-    line = "Mean sequence length: " + str(mean_length) + "\n"
-    f.writelines(line)
-    line = "Number of sequences: " + str(number_seq) + "\n"
-    f.writelines(line)
-    line = "\n"
-    f.writelines(line)
-    line = "Tags:\n"
-    f.writelines(line)
-    for tag in sorted(tag_dict):
-        line = tag + "\t" + str(round(tag_dict[tag] / sum(tag_dict.values()) * 100, 2)) + "%" + "\n"
+    with open(output_file, "w", encoding="utf-8") as f:
+        line = "Max sequence length: " + str(max_length) + "\n"
         f.writelines(line)
+        line = "Min sequence length: " + str(min_length) + "\n"
+        f.writelines(line)
+        line = "Mean sequence length: " + str(mean_length) + "\n"
+        f.writelines(line)
+        line = "Number of sequences: " + str(number_seq) + "\n"
+        f.writelines(line)
+        line = "\n"
+        f.writelines(line)
+        line = "Tags:\n"
+        f.writelines(line)
+        for tag in sorted(tag_dict):
+            line = tag + "\t" + str(round(tag_dict[tag] / sum(tag_dict.values()) * 100, 2)) + "%" + "\n"
+            f.writelines(line)
+
+if __name__ == "__main__":
+    main()
