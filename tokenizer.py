432a87b02bfa31d93d7b8696b9c56728006adf52
itsLuisa <lu.geiger3011@gmail.com>
2021-03-16 10:32:56 +0100

    method for tokenizing pos-tags as well

--- tokenizer.py
+++ tokenizer.py
@@ -20,6 +20,24 @@ def tokenize_and_encode(split, data_sets):
             all_encodings.append(encoded)
     return all_encodings
 
+def encode_pos(split, data_sets):
+    tz = BertTokenizer.from_pretrained("bert-base-cased")
+    all_encodings = list()
+    for sentence in data_sets[split]["pos_tags"]:
+        if sentence:
+            encoded = tz.encode_plus(
+                text=sentence,
+                is_split_into_words=True,
+                add_special_tokens=True,
+                truncation=True,
+                max_length=73,
+                padding="max_length",
+                return_attention_mask=True,
+                return_tensors="pt"
+            )
+            all_encodings.append(encoded)
+    return all_encodings
+
 def main():
     training_file = sys.argv[1]
     test_file = sys.argv[2]
@@ -37,7 +55,12 @@ def main():
     all_encodings_test = tokenize_and_encode("test", data_sets)
     all_encodings_validation = tokenize_and_encode("validation", data_sets)
 
-    print(all_encodings_validation)
+    pos_encoded_train = encode_pos("train", data_sets)
+    pos_encoded_test = encode_pos("test", data_sets)
+    pos_encoded_validation = encode_pos("validation", data_sets)
+
+    print(all_encodings_validation[0])
+    print(pos_encoded_validation[0])
 
 if __name__ == "__main__":
     main()
